19b05f2b91ac24f39a29070f993871ffe5e2fafa
Aayush Mahapatra <dr.aaaaaaaa8@gmail.com>
2020-09-13 20:30:53 +0530

    Updates

---
+++ precache-manifest.489d0327146ba468d0a141d0fce7c3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a140f0c91056a4a46bbb61299f30778",
+    "url": "/Dr-Apex/React-Video/index.html"
+  },
+  {
+    "revision": "f217be6f667521d9746e",
+    "url": "/Dr-Apex/React-Video/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "f8ac0bd05aeff778f3b9",
+    "url": "/Dr-Apex/React-Video/static/js/2.20d318ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dr-Apex/React-Video/static/js/2.20d318ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f217be6f667521d9746e",
+    "url": "/Dr-Apex/React-Video/static/js/main.c3bd1185.chunk.js"
+  },
+  {
+    "revision": "21da970d9c00b45e9463",
+    "url": "/Dr-Apex/React-Video/static/js/runtime-main.30782722.js"
+  }
+]);
